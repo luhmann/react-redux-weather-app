c166d0d4f2712a3fbc25c4b51a6e4c9b908a55aa
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-06-30 11:35:56 +0200

    refactor: Remove obsolete about-page

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from './home'
-import About from './about'
 import ConnectedCities from './cities'
 
 const App = () =>
   <div className="content">
     <header>
       <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
       <Link to="/cities">Cities</Link>
     </header>
 
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
-      <Route exact path="/cities" component={ConnectedCities} />
-    </main>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/cities" component={ConnectedCities} />
   </div>
 
 export default App
