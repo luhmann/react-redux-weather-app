60290d73f0f4d907498cd13bd3ded926219ddefa
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-07-04 20:39:55 +0200

    feat: replace custom loading animation with react-hold

--- src/components/city.js
+++ src/components/city.js
@@ -1,15 +1,8 @@
 import React from 'react'
+import hold from 'react-hold'
 import PropTypes from 'prop-types'
 
-const City = ({ degrees, iconUrl = null, name, isLoading = false }) => {
-  if (isLoading) {
-    return (
-      <div className="city">
-        Loading data for {name}
-      </div>
-    )
-  }
-
+const City = ({ degrees, iconUrl = null, name }) => {
   return (
     <div className="city">
       <div className="city__name">
@@ -35,4 +28,4 @@ City.defaultProps = {
   degrees: null,
 }
 
-export default City
+export default hold(City, props => props.isLoading)
